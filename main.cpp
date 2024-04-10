e95df0d6dda7a06024f9e97b80568ee482370158
peytonk132 <128337367+peytonk132@users.noreply.github.com>
2024-04-10 06:24:50 -0400

    This is an updated main.cpp file. I've never reall y used git before now so this is still sort of new for me.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,16 @@ using namespace std;
 
 int main()
 {
+
+    float valA;
+    float valB;
+
     cout << "Enter your number for A\n: ";
-    cin >> 
+    cin >> valA;
+
+    cout << "Enter your number for A\n: ";
+    cin >> valB;
+
+    cout << "you sum is: " << valA + valB << "\n";
+
 }
