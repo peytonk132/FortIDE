c86dd2a8c2395969cf5b717cfdb57876d7212099
peytonk132 <128337367+peytonk132@users.noreply.github.com>
2024-04-11 15:43:09 -0400

    create an accesspoint for FileExplorer.

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,51 @@
 #include "imgui.h"
 #include "imgui-SFML.h"
+#include "FileSystem/FileMan.h"
 
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Clock.hpp>
 #include <SFML/Window/Event.hpp>
 
-int main() {
+int main() 
+{
     sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
     window.setFramerateLimit(60);
     ImGui::SFML::Init(window);
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
     sf::Clock deltaClock;
-    while (window.isOpen()) {
+    while (window.isOpen()) 
+    {
         sf::Event event;
-        while (window.pollEvent(event)) {
+        while (window.pollEvent(event)) 
+        {
             ImGui::SFML::ProcessEvent(window, event);
 
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed) 
+            {
                 window.close();
             }
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
 
-        ImGui::ShowDemoWindow();
-
         ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenuBar())
+            {
+                if (ImGui::MenuItem("Open", "Ctrl+O")) 
+                {
+                    FileMan open;
+                    open.fileOpen();
+                }
+                ImGui::EndMenuBar();
+            }
+            ImGui::EndMainMenuBar();
+        }
         ImGui::End();
 
         window.clear();
-        window.draw(shape);
         ImGui::SFML::Render(window);
         window.display();
     }
