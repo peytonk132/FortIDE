60b203ceff32b4979da9aa94cafa3aaa7930c478
Peyton Kelly <peytonk1945@gmail.com>
2026-01-05 20:24:24 -0500

    Trying something experimental

---
+++ src/mainfunc/git/git_panel.cpp
@@ -0,0 +1,7 @@
+#include "git_panel.h"
+
+void Git::git_panel(){
+    ImGui::Begin("##", NULL, 0);
+    ImGui::
+    ImGui::End();
+}
---
+++ src/mainfunc/git/git_panel.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <imgui.h>
+
+class Git {
+public:
+    void git_panel();
+private:
+};
